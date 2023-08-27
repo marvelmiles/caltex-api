68f6d2f9aa7d38b98e00a4b862b7692c5e9014aa
marvelmiles <marvellousabidemi2@gmail.com>
2023-08-27 21:41:59 +0100

    added transaction model

---
+++ models/Transaction.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const schema = new mongoose.Schema(
+  {
+    action: {
+      type: String,
+      enum: ["deposit", "withdrawal"],
+      required:
+        "Transaction deposit is required. Expect either deposit or withdrawal"
+    },
+    status: {
+      type: String,
+      enum: ["processing", "approved", "rejected"],
+      required:
+        "Transaction status is required. Expect either processing, approved or rejected"
+    },
+    investment: {
+      type: String,
+      ref: "investment",
+      required: "Transaction investment id is required."
+    }
+  },
+  {
+    collection: "transaction",
+    timestamps: true,
+    versionKey: false,
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+      }
+    }
+  }
+);
+
+export default mongoose.model("transaction", schema);
