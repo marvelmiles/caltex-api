c756bd841392b71781929ca91870a1ee11786eee
marvelmiles <marvellousabidemi2@gmail.com>
2023-08-28 17:00:40 +0100

    added create-crytpo-order

--- routers/transaction.js
+++ routers/transaction.js
@@ -1,13 +1,15 @@
 import express from "express";
 import {
   createPayPalOrder,
-  capturePayPalOrder
+  capturePayPalOrder,
+  createCryptoOrder
 } from "../controllers/transaction";
 
 const transactionRouter = express.Router();
 
 transactionRouter
   .post("/create-paypal-order", createPayPalOrder)
-  .post("/capture-paypal-order", capturePayPalOrder);
+  .post("/capture-paypal-order", capturePayPalOrder)
+  .post("/create-crypto-order", createCryptoOrder);
 
 export default transactionRouter;
