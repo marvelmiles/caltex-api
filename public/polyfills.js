cd7ccd312d092e12bba90d436b44b200a0e29b07
marvelmiles <marvellousabidemi2@gmail.com>
2023-09-01 17:34:59 +0100

    modified polyfills.js

--- public/polyfills.js
+++ public/polyfills.js
@@ -1,5 +1,20 @@
-window.baseUrl =
-  window.location.hostname === "localhost" ||
-  window.location.hostname.indexOf("https") === -1
-    ? "http://localhost:8080"
-    : "https://caltex-api.onrender.com";
+function isLocalhost(hostname = window.location.hostname) {
+  hostname = hostname.toLowerCase();
+
+  if (hostname === "localhost") return true;
+
+  // Define an array of common domain extensions
+  const domainExtensions = ["com", "net", "org", "edu"];
+
+  for (const extension of domainExtensions) {
+    if (hostname.endsWith(`.${extension}`)) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+window.baseUrl = isLocalhost()
+  ? "http://localhost:8080"
+  : "https://caltex-api.onrender.com";
