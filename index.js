aba5289df2e2a61a0ab3c6d2b9c5c6de9af5884c
Glitch (glory-shelled-crow) <none>
2024-07-05 13:23:25 +0000

    🍢👓 Checkpoint ./index.js:68840956/63 ./package.json:68840956/2

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ import userRouter from "./routers/user";
 import transactionRouter from "./routers/transaction";
 import queryType from "query-types";
 import { isProdMode } from "./config/constants";
-import timeout from "connect-timeout";
 import { errHandler } from "./middlewares";
 import miscRouter from "./routers/misc";
 import postRouter from "./routers/post";
@@ -58,7 +57,6 @@ app
   .use(express.urlencoded({ extended: true }))
   .use(queryType.middleware())
   .use(express.static("public"))
-  .use(timeout("60s"));
 
 // ROUTES
 
